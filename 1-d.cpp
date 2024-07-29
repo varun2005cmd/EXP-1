51578bfd2e9c29aa0caefc503c6384db60fe1c87
varun2005cmd <136258677+varun2005cmd@users.noreply.github.com>
2024-07-29 16:08:28 +0530

    Create 1-d.cpp

---
+++ 1-d.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+// Varun Pendem PRN: 23070123149
+using namespace std;
+int main()
+{
+      int year;
+      cout<<endl<<"Enter the year";
+      cin>>year;
+      if(year%400==0 and year%100!=0 or year%4==0){
+            cout<<"LEAP year";
+      }
+      else{
+            cout<<"Non leap year";
+      }
+      return 0;
+}
