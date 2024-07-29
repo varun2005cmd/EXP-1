bd41bbae8d9e1406088c87de034f9233dde15871
varun2005cmd <136258677+varun2005cmd@users.noreply.github.com>
2024-07-29 16:07:54 +0530

    Create 1-c.cpp

---
+++ 1-c.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+// Varun Pendem PRN: 23070123149
+using namespace std;
+int main()
+{
+      int n;
+      cout<<"Enter n"<<endl;
+      cin>>n;
+
+      if(n%2==0){
+            cout<<endl<<"even";
+      }
+      else{
+            cout<<endl<<"odd";
+      }     
+      return 0;
+}
